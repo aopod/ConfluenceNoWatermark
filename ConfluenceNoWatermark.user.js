e7c6dffed670d7367b10c5c36e1322a01d5a6de8
aopod <aopodcom@gmail.com>
2018-11-25 12:43:23 +0800

    Add tampermonkey script

---
+++ ConfluenceNoWatermark.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Confluence No Watermark
+// @namespace    https://aopod.com/
+// @homepage     https://aopod.com/
+// @version      0.1
+// @description  Remove watermarks on Confluence Wiki
+// @author       aopod
+// @match        *REPLACE MACH HERE*
+// @grant        none
+// ==/UserScript==
+
+/*
+ * Notice: Please replace @match to pattern like: https://wiki.aopod.com/*
+ */
+
+(function(document) {
+    'use strict';
+
+    const removeAllMasks = () => {
+        const masks = document.querySelectorAll('.mask_div');
+        for (const mask of masks) {
+            if (mask.id.indexOf('mask_div') == 0) {
+                mask.parentNode.removeChild(mask);
+            }
+        }
+    };
+    const observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type == 'childList') {
+                const mask = document.querySelector('.mask_div');
+                if (mask) {
+                    removeAllMasks();
+                }
+                break;
+            }
+        }
+    });
+    observer.observe(document.body, {childList: true});
+})(document);
